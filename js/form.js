5b6c3c76ba6fc718ff0bbed44d96fd35cbe10ee8
AlenFarky <farky.dev@gmail.com>
2024-11-21 01:17:42 +0100

    Update form.js

--- js/form.js
+++ js/form.js
@@ -1,4 +1,4 @@
-document.getElementById('contact-form').addEventListener('submit', function(event) {
+document.getElementById('contact-form').addEventListener('submit', function (event) {
   event.preventDefault();
 
   const submitButton = document.querySelector('button[type="submit"]');
@@ -12,26 +12,41 @@ document.getElementById('contact-form').addEventListener('submit', function(even
     return;
   }
 
-  fetch(this.action, {
-    method: 'POST',
-    body: new FormData(this),
-    headers: { 'Accept': 'application/json' }
-  })
-  .then(response => {
-    if (response.ok) {
-      localStorage.setItem('lastSubmissionTime', currentTime);
-      showAlert("Thanks for reaching out!", "Expect a response shortly.", "success", submitButton)
-        .then(() => {
-          document.getElementById('contact-form').reset();
-          submitButton.disabled = false;
+  // Fetch the user's IP address
+  fetch('https://api.ipify.org?format=json')
+    .then(response => response.json())
+    .then(data => {
+      const ipAddress = data.ip;
+      const formData = new FormData(this);
+
+      // Append the IP address to the form data
+      formData.append('IP Address', ipAddress);
+
+      fetch(this.action, {
+        method: 'POST',
+        body: formData,
+        headers: { 'Accept': 'application/json' }
+      })
+        .then(response => {
+          if (response.ok) {
+            localStorage.setItem('lastSubmissionTime', currentTime);
+            showAlert("Thanks for reaching out!", "Expect a response shortly.", "success", submitButton)
+              .then(() => {
+                document.getElementById('contact-form').reset();
+                submitButton.disabled = false;
+              });
+          } else {
+            showAlert("Oops!", "We encountered an issue, please try again in a bit.", "error", submitButton);
+          }
+        })
+        .catch(error => {
+          showAlert("Oops!", "We encountered an issue, please try again in a bit.", "error", submitButton);
         });
-    } else {
-      showAlert("Oops!", "We encountered an issue, please try again in a bit.", "error", submitButton);
-    }
-  })
-  .catch(error => {
-    showAlert("Oops!", "We encountered an issue, please try again in a bit.", "error", submitButton);
-  });
+    })
+    .catch(error => {
+      showAlert("Oops!", "Could not fetch your IP address. Please try again later.", "error", submitButton);
+      submitButton.disabled = false;
+    });
 
   function showAlert(title, text, icon, button) {
     return swal({
